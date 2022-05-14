1197ba23373687cb466339d06aae7b06d317ef36
amns13 <slimmsady@gmail.com>
2022-05-15 02:26:03 +0530

    default player cards can be empty list instead of None

--- src/cards/players.py
+++ src/cards/players.py
@@ -6,7 +6,7 @@ from twentynine import TwentyNineCard
 class Player:
     def __init__(self, name):
         self.name = name
-        self.cards: list[TwentyNineCard] | None = None
+        self.cards: list[TwentyNineCard] = []
 
     def __repr__(self) -> str:
         return self.name
